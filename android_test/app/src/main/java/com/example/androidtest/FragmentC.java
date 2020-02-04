1e4c45e26c523a7aac316ec8ae9bd35a5df0bf5f
Eui Ryeong <37531737+Deplim@users.noreply.github.com>
2020-02-04 17:22:30 +0900

    .

---
+++ android_test/app/src/main/java/com/example/androidtest/FragmentC.java
@@ -0,0 +1,21 @@
+package com.example.androidtest;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class FragmentC extends Fragment {
+    public FragmentC() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.fragment_c, container, false);
+    }
+}
---
+++ android_test/app/src/main/java/com/example/androidtest/MainActivity.java
@@ -0,0 +1,164 @@
+package com.example.androidtest;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+import androidx.viewpager.widget.ViewPager;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.navigation.NavigationView;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.tabs.TabLayout;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private AppBarConfiguration mAppBarConfiguration;
+    Toolbar toolbar;
+    ViewPager pager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        // 탭 레이아블웃
+
+        TabLayout tabs = (TabLayout) findViewById(R.id.tabs);
+        tabs.addTab(tabs.newTab().setText("통화기록"));
+        tabs.addTab(tabs.newTab().setText("스팸기록"));
+        tabs.addTab(tabs.newTab().setText("연락처"));
+
+        tabs.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                int position = tab.getPosition();
+                Log.d("MainActivity", "선택된 탭 : " + position);
+                if (position == 0) {
+                    toolbar.setBackgroundColor(Color.RED);
+                } else if (position == 1) {
+                    toolbar.setBackgroundColor(Color.GREEN);
+                } else if (position == 2) {
+                    toolbar.setBackgroundColor(Color.BLUE);
+                }
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+        //뷰페이저
+        pager = (ViewPager) findViewById(R.id.pager);
+        pager.setOffscreenPageLimit(3);
+
+        MoviePagerAdapter adapter = new MoviePagerAdapter(getSupportFragmentManager());
+
+        FragmentA fragment1 = new FragmentA();
+        adapter.addItem(fragment1);
+
+        FragmentB fragment2 = new FragmentB();
+        adapter.addItem(fragment2);
+
+        FragmentC fragment3 = new FragmentC();
+        adapter.addItem(fragment3);
+
+        pager.setAdapter(adapter);
+
+
+        //드로어 네비게이션
+        setSupportActionBar(toolbar);
+
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+
+        mAppBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow,
+                R.id.nav_tools, R.id.nav_share, R.id.nav_send)
+                .setDrawerLayout(drawer)
+                .build();
+
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
+        NavigationUI.setupWithNavController(navigationView, navController);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
+                || super.onSupportNavigateUp();
+    }
+}
+
+class MoviePagerAdapter extends FragmentStatePagerAdapter {
+    ArrayList<Fragment> items = new ArrayList<Fragment>();
+
+    public MoviePagerAdapter(@NonNull FragmentManager fm) {
+        super(fm);
+    }
+
+    public void addItem(Fragment item) {
+        items.add(item);
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        return items.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return items.size();
+    }
+
+    //타이틀스크립
+    public CharSequence getPageTitle(int position) {
+        return "페이지 " + position;
+    }
+}
